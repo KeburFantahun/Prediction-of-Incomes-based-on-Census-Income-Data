5867c6f39c047c1c17ff8b4b2e7d9a405db4556c
fkebur@yahoo.com <fkebur@yahoo.com>
2021-03-29 20:21:01 -0500

    Choose adult dataset and begin project work

--- adult.r
+++ adult.r
@@ -1,10 +1,24 @@
 # adult data
+install.packages("aod")
+library(aod)
+library(ggplot2)
+
 # setwd("C:/Users/fkebu/Desktop") for adult data
-aData=read.csv("C:/Users/fkebu/Desktop/adult.data", header = FALSE)
-head(aData)
-summary(aData)
+adult=read.csv("C:/Users/fkebu/Desktop/adult.data", header = FALSE)
+head(adult)
+summary(adult)
 # "missing" data in the V2 and V14 columns/workclass and native country
 
 # names folder; contains data description and more
 # "C:/Users/fkebu/Desktop/adult.names"
 adultTest=read.csv("C:/Users/fkebu/Desktop/adult.test", header = FALSE, as.is = 1)
+summary(adultTest)
+
+# apply standard deviation to each ("2")column for the bank dataset
+apply(adult, 2,sd)
+
+# fully saturated model
+logitAFull <- glm(V15 ~ ., data = adult, family = "binomial")
+logitAFull
+summary(logitAFull)
+
--- glow.r
+++
@@ -1,6 +0,0 @@
-# glow bonemed data
-install.packages("aplore3")
-library(aplore3)
-?glow_bonemed
-head(glow_bonemed, n = 10)
-summary(glow_bonemed)
